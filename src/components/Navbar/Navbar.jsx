e54172cf2082be9d5a77b9349379da5a00102c2d
Raqz15 <Rajhans1501@gmail.com>
2025-02-16 18:57:54 +0530

    raj commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,38 +1,60 @@
 import React, { useState } from 'react';
-import { FaSearch, FaBell, } from 'react-icons/fa';
+import { FaSearch, FaBell, FaSun, FaMoon } from 'react-icons/fa'; 
 import profileImage from '../../assests/profile.jpg';
 
 const Navbar = () => {
-
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+    document.body.classList.toggle('dark', !isDarkMode); 
+  };
   return (
-    <div className='navbar h-13 w-100%  top-0 sticky z-10 flex justify-between items-center px-10'>
-      <div className="nav_left ">
-        <div className="search_box flex items-center h-10  mr-15 pr-5 pl-5 text-white bg-black rounded-full">
-          <input type="text" className='border-none outline-0 text-white placeholder:text-white' placeholder='Search'/>
-          <FaSearch className='cursor-pointer'/>
-        </div>
+    <div
+      className={`navbar w-full h-16 top-0 sticky z-10 flex justify-between items-center px-4 sm:px-8 lg:px-16 ${
+        isDarkMode ? 'bg-gray-800 text-white' : 'bg-white text-black'
+      }`}
+    >
+      <div
+        className={`search_box hidden sm:flex items-center h-10 px-4 rounded-full ${
+          isDarkMode ? 'bg-black' : 'bg-gray-200'
+        }`}
+      >
+        <input
+          type="text"
+          className="border-none outline-none placeholder:text-gray-400 bg-transparent"
+          placeholder="Search..."
+        />
+        <FaSearch className="ml-2 cursor-pointer" />
       </div>
 
-      <div className="middle_left">
 
-      </div>
+      <div className="flex items-center space-x-4">
 
-      <div className="middle_right"></div>
-
-      <div className="nav_right flex items-center cursor-pointer">
-        <div className="notification flex items-center h-10 w-10 bg-gray-100 rounded-full mr-5 justify-center">
+        <div
+          className={`notification flex items-center justify-center h-10 w-10 rounded-full ${
+            isDarkMode ? 'bg-gray-700' : 'bg-gray-100'
+          }`}
+        >
           <FaBell />
         </div>
-        <div className="profile_details flex items-center ">
-          <div className='mr-3'>
-            <h3 className='text-base'>Kush Patel</h3>
-            <h4 className='text-xs'>@Kush224</h4>
-          </div>
-          <img src={profileImage} className='h-10 w-10 rounded-full' alt="" />
+        <div className="hidden sm:flex items-center space-x-2">
+          <img
+            src={profileImage}
+            className="h-10 w-10 rounded-full object-cover"
+            alt="Profile"
+          />
+        </div>
+        <div
+          className={`theme-toggle p-2 rounded-full cursor-pointer ${
+            isDarkMode ? 'bg-yellow-500' : 'bg-gray-300'
+          }`}
+          onClick={toggleTheme}
+        >
+          {isDarkMode ? <FaSun className="text-white" /> : <FaMoon />}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import { FaHome, FaEnvelope, FaBell, FaPlus, FaSignOutAlt } from 'react-icons/fa';
-import logoimage from '../../assests/profile.jpg';
+import { FaHome, FaEnvelope, FaBell, FaPlus, FaSignOutAlt, FaBookmark, FaFire, FaEdit } from 'react-icons/fa';
+import logoimage from '../../assests/vite.svg';
 
 const Sidebar = () => {
   return (
    
-    <div className="sidebar ml-2 mt-5 h-160 flex flex-col items-center text-white bg-[#131313] z-20  w-25 fixed rounded-2xl top-0 left-0 cursor-pointer">
-          <div className='p-5'>
+    <div className="sidebar ml-3 mt-8 h-180 flex flex-col items-center text-white bg-[#131313] z-20  w-18 fixed rounded-2xl top-0 left- cursor-pointer">
+          <div className='p-5 mt-5'>
             <img src={logoimage} className='h-10 w-10 rounded-full' alt="" />
           </div>
           <div className='p-5 text-xl'>
             <FaHome/>
           </div>
           <div className='p-5 text-xl'>
-            <FaEnvelope/>
+            <FaFire/>
           </div>
           <div className='p-5 text-xl'>
-            <FaBell/>
+            <FaBookmark/>
           </div>
           <div className='p-5 text-xl'>
-            <FaPlus/>
+            <FaEdit/>
           </div>
           <div className='p-10 mt-auto text-xl'>
             <FaSignOutAlt/>
